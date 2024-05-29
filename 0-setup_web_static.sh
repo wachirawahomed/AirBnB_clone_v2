7b1abebfc85f72ff51a27a91dabf6cf1ea89ecc9
wachirawahomed <wachirawahomed@gmail.com>
2024-05-29 21:10:58 +0300

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,12 +5,13 @@
 sudo apt-get update
 sudo apt-get -y update
 sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
 # Create necessary directories if they don't exist
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
 # Create a fake HTML file
-echo "<html>
+sudo echo "<html>
 <head>
 </head>
 <body>
@@ -25,7 +26,7 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 
 # Update Nginx configuration
-sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 server_name _;/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
 # Restart Nginx
 sudo service nginx restart
