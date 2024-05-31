62061136593c41f802ad4bc81cfa2de4c99e2e60
wachirawahomed <wachirawahomed@gmail.com>
2024-06-01 00:16:12 +0300

    2-do_deploy_web_static.py update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,50 +1,46 @@
 #!/usr/bin/python3
-"""Fabric script that distributes an archive to your web servers"""
+"""
+Fabric script that distributes an archive to your web servers
+"""
 
-from fabric.api import *
+from fabric.api import env, put, run
 from os.path import exists
-from datetime import datetime
+import os
 
 env.hosts = ['18.209.224.134', '35.168.7.197']
 env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers and deploys it"""
-
+    """
+    Distributes an archive to your web servers
+    """
     if not exists(archive_path):
         return False
 
     try:
-        # Upload the archive to the /tmp/ directory of the web server
-        put(archive_path, '/tmp')
+        # Upload the archive to /tmp/ directory of the web server
+        filename = archive_path.split('/')[-1]
+        folder_name = "/data/web_static/releases/" + filename.split('.')[0]
 
-        # Get the filename without extension
-        filename = archive_path.split('/')[-1].split('.')[0]
+        put(archive_path, "/tmp/")
 
-        # Create the destination directory
-        run('mkdir -p /data/web_static/releases/{}/'.format(filename))
+        # Extract the archive to /data/web_static/releases/<filename>
+        run("mkdir -p {}".format(folder_name))
+        run("tar -xzf /tmp/{} -C {}".format(filename, folder_name))
 
-        # Uncompress the archive to the destination directory
-        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'
-            .format(filename, filename))
+        # Delete the archive from the web server
+        run("rm /tmp/{}".format(filename))
 
-        # Remove the archive from the server
-        run('rm /tmp/{}.tgz'.format(filename))
+        # Move contents to parent folder and remove the extracted folder
+        run("mv {}/web_static/* {}".format(folder_name, folder_name))
+        run("rm -rf {}/web_static".format(folder_name))
 
-        # Move the contents of the uncompressed folder to a new location
-        run('mv /data/web_static/releases/{}/web_static/* '
-            '/data/web_static/releases/{}/'.format(filename, filename))
+        # Delete the current symbolic link
+        run("rm -rf /data/web_static/current")
 
-        # Remove the uncompressed folder
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(filename))
-
-        # Delete the symbolic link /data/web_static/current
-        run('rm -rf /data/web_static/current')
-
-        # Create a new symbolic link linked to the new version of your code
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-            .format(filename))
+        # Create new symbolic link linked to the new version of code
+        run("ln -s {} /data/web_static/current".format(folder_name))
 
         print("New version deployed!")
         return True
